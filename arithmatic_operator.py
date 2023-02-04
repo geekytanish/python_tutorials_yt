41b403165a501e81d269fc5f8d775791252e6a6c
Tanish Sharma <109678180+geekytanish@users.noreply.github.com>
2023-02-04 12:35:18 +0530

    Update arithmatic_operator.py

--- arithmatic_operator.py
+++ arithmatic_operator.py
@@ -1,10 +1,10 @@
 a=5
 b=3
 
-print("a+b=",(a+b))
-print("a-b=",(a-b))
-print("a/b=",(a/b))
-print("a*b=",(a*b))
-print("a%b=",(a%b))
-print("a//b=",(a//b))
-print("a**b=",(a**b))
+print("a+b=",(a+b)) #addition
+print("a-b=",(a-b)) #subtraction
+print("a/b=",(a/b)) #division
+print("a*b=",(a*b)) #multiplication
+print("a%b=",(a%b)) #modulo
+print("a//b=",(a//b)) #divide-divide
+print("a**b=",(a**b)) #a raised to power b
