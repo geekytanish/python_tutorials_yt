c82b7d956a90692a274afd2aee7e86b7066114d1
Tanish Sharma <109678180+geekytanish@users.noreply.github.com>
2023-02-04 12:26:34 +0530

    Create printing_class_of_datatype.py

---
+++ printing_class_of_datatype.py
@@ -0,0 +1,17 @@
+name="Tanish"  #this is a string
+print(name, type(name))
+
+marks_maths=88  #this is an integer
+print(marks_maths, type(marks_maths))
+
+overall_GPA=8.9 #this is a float
+print(overall_GPA, type(overall_GPA))
+
+is_passed = True  #this is a boolean
+print(is_passed, type(is_passed))
+
+my_complex_number= 1 + 4j  #1 is real and 4 is imaginary
+print(my_complex_number, type(my_complex_number))
+
+fruit_list=['Apple','Banana', 'Kiwi'] #this is a list
+print(fruit_list, type(fruit_list))
