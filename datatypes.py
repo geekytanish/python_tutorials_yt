198d859b6da5354a273744c040dfc6d7a35166e8
Tanish Sharma <109678180+geekytanish@users.noreply.github.com>
2023-02-04 12:21:48 +0530

    Create datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,7 @@
+name="Tanish"  #this is a string
+marks_maths=88  #this is an integer
+overall_GPA=8.9 #this is a float
+
+is_passed = true  #this is a boolean 
+my_complex_number= 1 + 4j  #1 is real and 4 is imaginary
+fruit_list=['Apple','Banana', 'Kiwi'] #this is a list
