336c38110eca2e464857bd0e9304e464cafea963
Tanish Sharma <109678180+geekytanish@users.noreply.github.com>
2023-02-04 12:38:46 +0530

    Create comments.py

---
+++ comments.py
@@ -0,0 +1,22 @@
+#Rules for writing identifiers:
+#Use only alphanumeric characters and underscores.
+#Never start with a number
+#Use underscores to separate two words
+#Keywords cannot be identifiers
+
+
+'''
+Rules for writing identifiers:
+Use only alphanumeric characters and underscores.
+Never start with a number
+Use underscores to separate two words
+Keywords cannot be identifiers
+'''
+
+"""
+Rules for writing identifiers:
+Use only alphanumeric characters and underscores.
+Never start with a number
+Use underscores to separate two words
+Keywords cannot be identifiers
+"""
