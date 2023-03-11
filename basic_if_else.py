50342fb4a3234b596e7d82350d93a7fd74f2d9c7
Tanish Sharma <109678180+geekytanish@users.noreply.github.com>
2023-03-11 17:26:36 +0530

    Update basic_if_else.py

--- basic_if_else.py
+++ basic_if_else.py
@@ -1,7 +1,9 @@
 age=input("What is your age?")
 check=input("Do you have adhaar Card? Press 1 for yes and 0 for no")
 
-if int(age)>=18 and bool(check)==1:
+if int(age)>=18 and int(check)==1:
     print("Yes, you can vote")
+elif int(age)>=18 and int(check)==0:
+    print("Get your Adhaar card first")
 else:
     print("nikal")
